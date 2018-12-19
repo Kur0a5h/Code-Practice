87e6a09e3f9a5f36fed160496e49298c745d0572
Kur0a5h <kuroash.esmaili@gmail.com>
2018-12-19 12:26:08 -0800

    last commit didnt have the pseudo

---
+++ pascals-triangle/main.js
@@ -0,0 +1,11 @@
+//dynamically create divs for each block of the triangle
+//triangle is comprised of rows and length of triangles sides=input
+//use flexbox? with an offset for the beginning of each row to form 
+//the triangle, wrapping
+
+//Determine the value of each component and store in order in an array--
+//if the components of the triangle 
+//are lined up in an array you move back 
+//on the array the length of the components row 
+//and add that value to the value to its right to
+//find the value of the current component
