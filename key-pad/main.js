04b917d68a6663159fb5f8f89504e9199a4f25e8
Kur0a5h <kuroash.esmaili@gmail.com>
2019-01-09 11:14:59 -0800

    solved with one number at a time

---
+++ key-pad/main.js
@@ -0,0 +1,37 @@
+// $(document).ready(()=>{
+
+// })
+const keypad=[['a','b','c'],['d','e','f'],['g','h','i'],['j','k','l'],['m','n','o'],['p','q','r','s'],['t','u','v'],['w','x','y','z']];
+var output=[];
+
+
+function keypressConcat(num){
+    letterArrayIndex=num-2;
+    letterArray=keypad[letterArrayIndex];
+    var newOutput=[];
+    if(output.length===0){
+    for(i=0;i<letterArray.length;i++){
+        
+        output.push(letterArray[i]);
+    }
+    }else{
+        for(i=0;i<output.length;i++){
+            for(j=0;j<letterArray.length;j++){
+                letterConcat=output[i]+letterArray[j];
+                newOutput.push(letterConcat);
+                
+
+            }
+            
+        }
+    }
+    
+    if(newOutput===0){
+        console.log(output)
+    return output;
+    }else{
+        output=newOutput;
+        console.log(output)
+        return output;
+    }
+}
